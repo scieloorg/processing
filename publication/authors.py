2fdd2a8f903b59c53d26fac341ee3c82ad600358
fabiobatalha <fabiobatalha@gmail.com>
2015-07-14 10:55:47 -0300

    Relatório de Autores e afiliações

---
+++ publication/authors.py
@@ -0,0 +1,152 @@
+# coding: utf-8
+"""
+Este processamento gera uma tabulação de autores e afiliação de cada artigo da
+coleção SciELO.
+Formato de saída:
+"PID","issn","título","área temática","ano de publicação","tipo de documento","autor","paises de afiliação","Instituição"
+"""
+import argparse
+import logging
+import codecs
+
+import utils
+
+logger = logging.getLogger(__name__)
+
+def _config_logging(logging_level='INFO', logging_file=None):
+
+    allowed_levels = {
+        'DEBUG': logging.DEBUG,
+        'INFO': logging.INFO,
+        'WARNING': logging.WARNING,
+        'ERROR': logging.ERROR,
+        'CRITICAL': logging.CRITICAL
+    }
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    logger.setLevel(allowed_levels.get(logging_level, 'INFO'))
+
+    if logging_file:
+        hl = logging.FileHandler(logging_file, mode='a')
+    else:
+        hl = logging.StreamHandler()
+
+    hl.setFormatter(formatter)
+    hl.setLevel(allowed_levels.get(logging_level, 'INFO'))
+
+    logger.addHandler(hl)
+
+    return logger
+
+
+class Dumper(object):
+
+    def __init__(self, collection, issns=None, output_file=None):
+
+        self._ratchet = utils.ratchet_server()
+        self._articlemeta = utils.articlemeta_server()
+        self.collection = collection
+        self.issns = issns
+        self.output_file = output_file
+
+    def run(self):
+
+        header = [u"PID",u"ISSN",u"título",u"área temática",u"ano de publicação",u"tipo de documento",u"author",u"instituição",u"paises de afiliação",u"estado de afiliação",u"país de afiliação"]
+
+        if not self.issns:
+            self.issns = [None]
+
+        if not self.output_file:
+            print(header)
+            for issn in self.issns:
+                for data in self.get_data(issn=issn):
+                    print(self.fmt_csv(data))
+            exit()
+
+        with codecs.open(self.output_file, 'w', encoding='utf-8') as f:
+            f.write('%s\r\n' % ','.join(header))
+            for issn in self.issns:
+                for data in self.get_data(issn=issn):
+                    f.write('%s\r\n' % self.fmt_csv(data))
+        
+    def fmt_csv(self, data):
+        countries = set()
+
+        affs = {item['index'].upper():item for item in data.mixed_affiliations}
+
+        for author in data.authors:
+            line = [
+                data.publisher_id,
+                data.journal.scielo_issn,
+                data.journal.title,
+                ','.join(data.journal.subject_areas),
+                data.publication_date[0:4],
+                data.document_type,
+                ' '.join([author.get('given_names', ''), author.get('surname', '')])
+            ]
+            if 'xref' in author:
+                for index in author['xref']:
+                    line.append(affs.get(index, {}).get('institution', '')),
+                    line.append(affs.get(index, {}).get('country', '')),
+                    line.append(affs.get(index, {}).get('state', '')),
+                    line.append(affs.get(index, {}).get('city', ''))
+
+            joined_line = ','.join(['"%s"' % i.replace('"', '""') for i in line])
+            return joined_line
+
+    def get_data(self, issn):
+        for document in self._articlemeta.documents(collection=self.collection, issn=issn):
+
+            yield document
+
+
+def main():
+
+    parser = argparse.ArgumentParser(
+        description='Dump languages distribution by article'
+    )
+
+    parser.add_argument(
+        'issns',
+        nargs='*',
+        help='ISSN\'s separated by spaces'
+    )
+
+    parser.add_argument(
+        '--collection',
+        '-c',
+        help='Collection Acronym'
+    )
+
+    parser.add_argument(
+        '--output_file',
+        '-r',
+        help='File to receive the dumped data'
+    )
+
+    parser.add_argument(
+        '--logging_file',
+        '-o',
+        help='Full path to the log file'
+    )
+
+    parser.add_argument(
+        '--logging_level',
+        '-l',
+        default='DEBUG',
+        choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+        help='Logggin level'
+    )
+
+    args = parser.parse_args()
+    _config_logging(args.logging_level, args.logging_file)
+    logger.info('Dumping data for: %s' % args.collection)
+
+    issns = None
+    if len(args.issns) > 0:
+        issns = utils.ckeck_given_issns(args.issns)
+
+    dumper = Dumper(args.collection, issns, args.output_file)
+
+    dumper.run()
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ tests_require = []
 
 setup(
     name="processing-scielo",
-    version="0.1.11",
+    version="0.1.12",
     description="SciELO processing modules for analytics, access statistics, etc",
     author="SciELO",
     author_email="scielo-dev@googlegroups.com",
@@ -36,6 +36,7 @@ setup(
     processing_accesses_dumpdata=accesses.dumpdata:main
     processing_publication_languages=publication.languages:main
     processing_publication_affiliations=publication.affiliations:main
+    processing_publication_authors=publication.authors:main
     processing_publication_counts=publication.counts:main
     processing_publication_journals=publication.journals:main
     processing_publication_licenses=publication.licenses:main
