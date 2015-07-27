709c87e4d99a234f971172fc0fb22c7e655bf208
fabiobatalha <fabiobatalha@gmail.com>
2015-07-27 13:44:09 -0300

    Ajustes para excluir caracteres de controle do XML

--- export/xml_rsps.py
+++ export/xml_rsps.py
@@ -135,10 +135,15 @@ class Dumper(object):
         for issn in self.issns:
             for document in self._articlemeta.documents(collection=self.collection, issn=issn):
                 logger.debug('Reading document: %s' % document.publisher_id)
-                xml = self._articlemeta.document(code=document.publisher_id, collection=document.collection_acronym, fmt='xmlrsps')
-                validation_result = analyze_xml(xml, document)
-
-                print(self.fmt_json(document, validation_result))
+                try:
+                    xml = self._articlemeta.document(code=document.publisher_id, collection=document.collection_acronym, fmt='xmlrsps')
+                except:
+                    logger.exception('Got exception on xml production')
+                    validation_result = analyze_xml('', document)
+                    print(self.fmt_json(document, validation_result))
+                else:
+                    validation_result = analyze_xml(xml, document)
+                    print(self.fmt_json(document, validation_result))
 
 
 def main():
