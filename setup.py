ccd27259e1b417a126bd5d358d8ad81d2d2bb6f5
fabiobatalha <fabiobatalha@gmail.com>
2015-08-17 14:04:46 -0300

    DOAJ Export Processing

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ tests_require = []
 
 setup(
     name="processing",
-    version ="0.1.21",
+    version ="0.1.22",
     description="SciELO processing modules for analytics, access statistics, etc",
     author="SciELO",
     author_email="scielo-dev@googlegroups.com",
