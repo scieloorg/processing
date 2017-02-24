fe77438d15567fcd2896b22f22adb7dabb35da2b
fabiobatalha <fabiobatalha@gmail.com>
2017-02-24 14:00:41 -0300

    Ajuste em path do XSD DOAJ

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ tests_require = []
 
 setup(
     name="processing",
-    version="1.13.9",
+    version="1.13.10",
     description="SciELO processing modules for analytics, access statistics, etc",
     author="SciELO",
     author_email="scielo-dev@googlegroups.com",
