9972461be198230091b02d02384ea8bc84c0dd81
fabiobatalha <fabiobatalha@gmail.com>
2017-07-06 18:43:24 -0300

    Ajiste de versão

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ tests_require = []
 
 setup(
     name="processing",
-    version="1.16.10",
+    version="1.17.10",
     description="SciELO processing modules for analytics, access statistics, etc",
     author="SciELO",
     author_email="scielo-dev@googlegroups.com",
